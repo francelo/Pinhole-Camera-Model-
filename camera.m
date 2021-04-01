6ed4bbbd55218ffcc2e94a91401ab2bde6be02a9
AntoManuele <antomanuele.am@gmail.com>
2021-04-01 10:50:17 +0200

    update 1/04

--- camera.m
+++ camera.m
@@ -1,25 +1,21 @@
 close all, clear, clc;
 
+%% parameters
+% frame
 O = [0; 0; 0]; % origin
-C = [0.5; 0.5; 1.5]; % camera origin
-
-versor_origin = 0.5;
+C = [0.2; 0.3; 0.6]; % camera origin
+versor_origin = 0.4;
 versor_camera = 0.2;
-
 origin_axis = {'O';'X';'Y';'Z'};
 camera_axis = {'oc','zc','xc','yc'};
 
-grid on;
-quiver3([O(1);O(1);O(1)],[O(2);O(2);O(2)],[O(3);O(3);O(3)],[versor_origin;0;0],[0;versor_origin;0],[0;0;versor_origin]) % origin frame
-text([O(1),O(1)+versor_origin,O(1),O(1)], [O(2),O(2),O(2)+versor_origin,O(2)], [O(3),O(3),O(3),O(3)+versor_origin], origin_axis)
+% point (x,y,z)
+p = [1.0; 0.15; 0.8];
 
-axis equal
-hold on;
-quiver3([C(1);C(1);C(1)],[C(2);C(2);C(2)],[C(3);C(3);C(3)],[versor_camera;0;0],[0;versor_camera;0],[0;0;versor_camera]) % camera frame
-text([C(1),C(1)+versor_camera,C(1),C(1)], [C(2),C(2),C(2)+versor_camera,C(2)], [C(3),C(3),C(3),C(3)+versor_camera], camera_axis)
+% focal lenght
+f = 0.05;
 
-
-
+% 
 d = C - O;
 
 Rx = [1 0 0;
@@ -33,18 +29,42 @@ Ry = [cos(pi/2) 0 sin(pi/2);
 R = Rx*Ry;
 
 
-Tco = [R d;
+Twc = [R d;
     zeros(1,3) 1];
 
+p_cam = Twc^-1 * [p;1]
 
-%% P matrix
-
-f = 0.015; % focal lenght
+% camera projection matrix
 P = diag([f,f,1]) * [eye(3), zeros(3,1)];
 
-p = [4.6; 0.25; 1.8];
+p_tilde = P*[p;1];
+x_proj = p_tilde(1)/p(3)
+y_proj = p_tilde(2)/p(3)
+
+%% plot
+
+grid on;
+quiver3([O(1);O(1);O(1)],[O(2);O(2);O(2)],[O(3);O(3);O(3)],[versor_origin;0;0],[0;versor_origin;0],[0;0;versor_origin]) % origin frame
+text([O(1),O(1)+versor_origin,O(1),O(1)], [O(2),O(2),O(2)+versor_origin,O(2)], [O(3),O(3),O(3),O(3)+versor_origin], origin_axis)
+
+axis equal
+hold on;
+quiver3([C(1);C(1);C(1)],[C(2);C(2);C(2)],[C(3);C(3);C(3)],[versor_camera;0;0],[0;versor_camera;0],[0;0;versor_camera]) % camera frame
+text([C(1),C(1)+versor_camera,C(1),C(1)], [C(2),C(2),C(2)+versor_camera,C(2)], [C(3),C(3),C(3),C(3)+versor_camera], camera_axis)
+
 scatter3(p(1),p(2),p(3));
 scatter3(linspace(C(1),p(1)), linspace(C(2),p(2)), linspace(C(3),p(3)),' . ')
-xlim([-0.5, 5])
-ylim([-0.5, 2])
-zlim([0, 2.5])
+
+
+x=linspace(C(1)+f,C(1)+f,20);
+y=linspace(C(2)-0.25,C(2)+0.25,20);
+z=linspace(C(3)-0.18,C(3)+0.18,20);
+
+[X, Y] = meshgrid(x,y);
+Z = meshgrid(z);
+s = surf(X,Y,Z,'FaceAlpha',0.3);
+s.EdgeColor = 'none';
+
+% xlim([-2.5, 5])
+% ylim([-2.5, 3])
+% zlim([0, 3.5])

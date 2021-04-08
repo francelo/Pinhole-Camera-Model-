a3aa6ba47939488675f797a3cb44d86eb8c9eb53
AntoManuele <antomanuele.am@gmail.com>
2021-04-08 18:23:27 +0200

    update IBVS plot

--- IBVS/init.m
+++ IBVS/init.m
@@ -6,11 +6,16 @@ O = [0; 0; 0];        % world origin frame
 C = [0.2; 0.3; 0.6];  % camera origin frame
 
 f = 0.05;             % focal lenght
-p = [1.0; 0.15; 0.8]; % point's position (x,y,z) in world frame
+
 d = C - O;            % distance between camera and world frame
 ang = [pi/2 pi/2 0]; 
 init_pose = [C ; ang'];
 frequency = 200;       % sampling frequency [Hz]
 delta_t = 1/frequency; % sampling time [s]
-Kp = eye(2);
 
+% points' position (x,y,z) in world frame
+p1 = [1.2; 0.45; 0.8]; 
+p2 = [1.2; 0.45; 0.8]; 
+
+Kp = 10 * eye(4);
+Kd = 0 * eye(4);

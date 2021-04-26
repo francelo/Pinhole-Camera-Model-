5565a9ad926ded909e3beb3d9e3cc84e1df161e9
AntoManuele <antomanuele.am@gmail.com>
2021-04-26 09:56:42 +0200

    update 26/04

--- IBVS/plot_IBVS.m
+++ IBVS/plot_IBVS.m
@@ -11,16 +11,9 @@ p2_ = out.p(:, 4:6);
 
 %% plot point
 
- grid on; 
- quiver3([O(1);O(1);O(1)],[O(2);O(2);O(2)],[O(3);O(3);O(3)],[versor_origin;0;0],[0;versor_origin;0],[0;0;versor_origin]) % world frame
-% text([O(1),O(1)+versor_origin,O(1),O(1)], [O(2),O(2),O(2)+versor_origin,O(2)], [O(3),O(3),O(3),O(3)+versor_origin], origin_axis)
-% 
-% axis equal
-% hold on;
-% 
-% scatter3(p(1),p(2),p(3)); 
-% scatter3(linspace(C(1),p(1)), linspace(C(2),p(2)), linspace(C(3),p(3)),' . ')
-
+grid on; 
+quiver3([O(1);O(1);O(1)],[O(2);O(2);O(2)],[O(3);O(3);O(3)],[versor_origin;0;0],[0;versor_origin;0],[0;0;versor_origin]) % world frame
+ 
 x_pose = out.pose(:, 1);
 y_pose = out.pose(:, 2);
 z_pose = out.pose(:, 3);
